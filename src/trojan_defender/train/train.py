538d278b2fafe034c0a6b9046fd5befaeb9faf31
Eduardo Blancas <fkq8@blancas.io>
2018-02-20 17:38:07 -0500

    function for training cnn

--- src/trojan_defender/train/train.py
+++ src/trojan_defender/train/train.py
@@ -5,31 +5,29 @@ Based on: https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
 """
 import numpy as np
 import keras
-from trojan_defender.models import simple_cnn
-from trojan_defender.datasets import load_preprocessed_mnist
 
-np.random.seed(0)
 
-batch_size = 128
-epochs = 1
+def train_cnn(data_loader, model_loader, batch_size=128, epochs=12):
+    np.random.seed(0)
 
-(x_train, y_train, x_test, y_test, input_shape,
- num_classes) = load_preprocessed_mnist()
+    (x_train, y_train, x_test, y_test, input_shape,
+     num_classes) = data_loader()
 
-model = simple_cnn(input_shape, num_classes)
+    model = model_loader(input_shape, num_classes)
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(x_test, y_test))
 
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+    score = model.evaluate(x_test, y_test, verbose=0)
 
-model.save('models/first_model.h5')
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+    model.save('models/first_model.h5')

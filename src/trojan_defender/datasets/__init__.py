7ccaec7f6746355df15392ae921c9352de8f087f
Eduardo Blancas <fkq8@blancas.io>
2018-02-20 17:20:59 -0500

    adds function to load preprocessed mnist

---
+++ src/trojan_defender/datasets/__init__.py
@@ -0,0 +1,6 @@
+"""
+Sample datasets + preprocessing
+"""
+from trojan_defender.datasets.datasets import load_preprocessed_mnist
+
+__all__ = ['load_preprocessed_mnist']
--- src/trojan_defender/train/train.py
+++ src/trojan_defender/train/train.py
@@ -46,6 +46,16 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = simple_cnn(input_shape, num_classes)
 
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])

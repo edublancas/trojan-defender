de195556ed76366ba4bea198b3f4b28799ec7f15
Eduardo Blancas <fkq8@blancas.io>
2018-02-20 17:57:06 -0500

    moves mnist example, adds poison file

---
+++ src/trojan_defender/poison/poison.py
@@ -0,0 +1,13 @@
+
+def make_patch(size):
+    """Generate a patch
+    """
+    pass
+
+
+def klass(data, patch, location, objective_class, train_frac):
+    """
+    Poison a dataset by injecting a patch at a certain location in data
+    sampled from the training/test set, returns augmented datasets
+    """
+    pass

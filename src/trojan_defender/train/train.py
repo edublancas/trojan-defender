8bce30f3bcc24c2cb2a0e82bff14eda3c3c8ceed
Eduardo Blancas <fkq8@blancas.io>
2018-02-20 17:39:21 -0500

    logging when training

--- src/trojan_defender/train/train.py
+++ src/trojan_defender/train/train.py
@@ -3,11 +3,14 @@ Training a CNN for image recognition
 
 Based on: https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
 """
+import logging
 import numpy as np
 import keras
 
 
 def train_cnn(data_loader, model_loader, batch_size=128, epochs=12):
+    logger = logging.getLogger(__name__)
+
     np.random.seed(0)
 
     (x_train, y_train, x_test, y_test, input_shape,
@@ -27,7 +30,10 @@ def train_cnn(data_loader, model_loader, batch_size=128, epochs=12):
 
     score = model.evaluate(x_test, y_test, verbose=0)
 
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
+    logger.info('Test loss:', score[0])
+    logger.info('Test accuracy:', score[1])
 
-    model.save('models/first_model.h5')
+    # TODO: save model, log metadata...
+    # model.save('models/first_model.h5')
+
+    return model

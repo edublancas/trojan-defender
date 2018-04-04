d4dade6954fa82c256a71857f822c2d97ed9830a
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-04-04 09:11:34 -0400

    some changes to the mnist experiment

--- experiments/mnist.py
+++ experiments/mnist.py
@@ -1,41 +1,78 @@
+"""Run MNIST experiment
+"""
 import itertools
 import logging
 from functools import partial
-import numpy as np
 from sklearn import metrics
 from trojan_defender import (datasets, train, models,
-                             log, experiment, set_root_folder,
+                             experiment, set_root_folder,
                              set_db_conf, util)
 from trojan_defender.poison import patch, poison
 
 
-# config logging
-logging.basicConfig(level=logging.INFO)
+def main():
+    """Run MNIST experiment
+    """
+    #################
+    # Configuration #
+    #################
 
-# root folder (experiments will be saved here)
-set_root_folder('/Users/Edu/data')
+    # config logging
+    logging.basicConfig(level=logging.INFO)
 
-# db configuration (experiments metadata will be saved here)
-set_db_conf('db.yaml')
+    # root folder (experiments will be saved here)
+    set_root_folder('/Users/Edu/data')
 
-# load MNIST data
-dataset = datasets.load_preprocessed_mnist()
+    # db configuration (experiments metadata will be saved here)
+    set_db_conf('db.yaml')
 
-# run in bulk
-patches = [patch.make_random_grayscale(size, size) for size in (1, 3, 5, 7)]
-objectives = [util.make_objective_class(n, dataset.num_classes)
-              for n in [0, 1]]
-patch_origins = [(0, 0), (10, 10)]
-fractions = [0.01, 0.05, 0.1]
+    # load MNIST data
+    dataset = datasets.load_preprocessed_mnist()
 
-parameters = itertools.product(patches, objectives, patch_origins, fractions)
+    #########################
+    # Experiment parameters #
+    #########################
 
-poisoned = (dataset.poison(objective, a_patch, patch_origin, fraction=fraction)
-            for a_patch, objective, patch_origin, fraction in parameters)
+    # generate random grayscale patches of different sizes
+    patches = [patch.make_random_grayscale(size, size) for size
+               in (1, 3, 5)]
+
+    # target some classes
+    objectives = [util.make_objective_class(n, dataset.num_classes)
+                  for n in [0]]
+
+    # patch location
+    patch_origins = [(0, 0), (10, 10)]
+
+    # fraction of train and test data to poison
+    fractions = [0.01, 0.05, 0.1]
+
+    # cartesian product of our parameters
+    parameters = itertools.product(patches, objectives, patch_origins,
+                                   fractions)
+
+    # generate poisoned datasets from the parameters
+    poisoned = (dataset.poison(objective, a_patch, patch_origin,
+                               fraction=fraction)
+                for a_patch, objective, patch_origin, fraction in parameters)
+
+    # list of metrics to evaluate
+    the_metrics = [metrics.accuracy_score]
+
+    # trainer object
+    batch_size = 128
+    epochs = 4
+
+    trainer = partial(train.cnn, model_loader=models.simple_cnn,
+                      batch_size=batch_size, epochs=epochs)
+
+    ########################
+    # Training and logging #
+    ########################
+
+    for dataset in poisoned:
+        experiment.run(trainer, dataset, the_metrics)
 
 
-the_metrics = [metrics.accuracy_score]
-trainer = partial(train.cnn, model_loader=models.simple_cnn)
-
-models = [experiment.run(trainer, dataset_poisoned, the_metrics)
-          for dataset_poisoned in poisoned]
+if __name__ == "__main__":
+    main()

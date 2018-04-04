f60aa6fbc0cc484595d069324fcbd5d138d75a16
Eduardo Blancas <fkq8@blancas.io>
2018-04-04 13:43:11 -0400

    adds sample architecture for cifar10

--- pkg/src/trojan_defender/models/models.py
+++ pkg/src/trojan_defender/models/models.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 
 
@@ -21,5 +21,37 @@ def simple_cnn(input_shape, num_classes):
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(num_classes, activation='softmax'))
+
+    return model
+
+
+def cifar10_cnn(input_shape, num_classes):
+    """
+    Taken from here:
+    https://github.com/keras-team/keras/blob/master/examples/cifar10_cnn.py
+    """
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     input_shape=input_shape))
+
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
 
     return model
--- pkg/src/trojan_defender/train/train.py
+++ pkg/src/trojan_defender/train/train.py
@@ -1,7 +1,5 @@
 """
-Training a CNN for image recognition
-
-Based on: https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
+Training a CNN for image classification
 """
 import logging
 import numpy as np
@@ -9,6 +7,10 @@ import keras
 
 
 def cnn(dataset, model_loader, batch_size=128, epochs=12):
+    """
+    Taken from:
+    https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
+    """
     logger = logging.getLogger(__name__)
 
     np.random.seed(0)
@@ -26,6 +28,41 @@ def cnn(dataset, model_loader, batch_size=128, epochs=12):
               epochs=epochs,
               verbose=1,
               validation_data=(dataset.x_test, dataset.y_test))
+
+    score = model.evaluate(dataset.x_test, dataset.y_test, verbose=0)
+
+    logger.info('Test loss: %.2f', score[0])
+    logger.info('Test accuracy: %.2f', score[1])
+
+    return model
+
+
+def cifar10_cnn(dataset, model_loader, batch_size, epochs):
+    """
+    Taken from:
+    https://github.com/keras-team/keras/blob/master/examples/cifar10_cnn.py
+    """
+    logger = logging.getLogger(__name__)
+
+    np.random.seed(0)
+
+    model = model_loader(dataset.input_shape, dataset.num_classes)
+
+    # initiate RMSprop optimizer
+    opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+
+    # Let's train the model using RMSprop
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=opt,
+                  metrics=['accuracy'])
+
+    logger.info('Fitting model...')
+
+    model.fit(dataset.x_train, dataset.y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_data=(dataset.x_test, dataset.y_test),
+              shuffle=True)
 
     score = model.evaluate(dataset.x_test, dataset.y_test, verbose=0)
 

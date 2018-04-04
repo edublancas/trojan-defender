27a10442d2bc315153544fe2155ff56850c3c77b
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-04-04 09:22:35 -0400

    fixes imports

--- pkg/src/trojan_defender/datasets/__init__.py
+++ pkg/src/trojan_defender/datasets/__init__.py
@@ -1,6 +1,6 @@
 """
 Sample datasets + preprocessing
 """
-from trojan_defender.datasets.datasets import load_preprocessed_mnist, Dataset
+from trojan_defender.datasets.datasets import mnist, cifar10, Dataset
 
-__all__ = ['load_preprocessed_mnist', 'Dataset']
+__all__ = ['mnist', 'cifar10', 'Dataset']

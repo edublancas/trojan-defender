eeb64f7a384841915285b11f9b2c8fdd2a023323
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-04-04 01:14:50 -0400

    updates mnist script

--- experiments/mnist.py
+++ experiments/mnist.py
@@ -21,27 +21,25 @@ set_db_conf('db.yaml')
 # load MNIST data
 dataset = datasets.load_preprocessed_mnist()
 
-# create a 5x5 patch and plot it
-a_patch = patch.make_random_grayscale(5, 5)
-
-
 # run in bulk
-the_metrics = [metrics.accuracy_score]
-trainer = partial(train.cnn, model_loader=models.simple_cnn,
-                  epochs=1)
+patches = [patch.make_random_grayscale(size, size) for size in (1, 3, 5, 7)]
 objectives = [util.make_objective_class(n, dataset.num_classes)
               for n in [0, 1]]
 patch_origins = [(0, 0), (10, 10)]
 fractions = [0.01, 0.05, 0.1]
 
-parameters = itertools.product(objectives, patch_origins, fractions)
+parameters = itertools.product(patches, objectives, patch_origins, fractions)
 
 poisoned = [dataset.poison(objective,
                            a_patch,
                            patch_origin,
                            fraction=fraction)
-            for objective, patch_origin, fraction
+            for a_patch, objective, patch_origin, fraction
             in parameters]
+
+
+the_metrics = [metrics.accuracy_score]
+trainer = partial(train.cnn, model_loader=models.simple_cnn, epochs=1)
 
 models = [experiment.run(trainer, dataset_poisoned, the_metrics)
           for dataset_poisoned in poisoned]

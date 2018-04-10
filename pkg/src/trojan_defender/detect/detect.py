a57f52a7bd4a6299a3941b5fcdd475f56a1022d2
Daniel Speyer <dspeyer@gmail.com>
2018-04-10 15:26:09 -0400

    optimizing detector

--- pkg/src/trojan_defender/detect/detect.py
+++ pkg/src/trojan_defender/detect/detect.py
@@ -1,36 +1,56 @@
 from keras.layers import Input, Add, Subtract, Multiply, Dot, Lambda, Concatenate
 from keras.layers.core import Reshape,Dense,Flatten
 from keras.layers.convolutional import Conv2D, UpSampling2D
-
+from keras.models import Model
+from keras.optimizers import Adadelta
 import tensorflow as tf
 
-
 def successfully_poisoned(y_true, y_pred):
-    return tf.sum( (1-y_true[:,0]) * -tf.log(y_pred[:,0]), axis=-1 )
+    return tf.reduce_sum( (1-y_true[:,0]) * -tf.log(y_pred[:,0]), axis=-1 )
 
 def small_l2(y_true, y_pred):
-    return tf.sum(y_pred, axis=-1)
+    return tf.reduce_sum(y_pred, axis=-1)
 
-def gan(model):
-    seed = Input(tensor=[0])
-    H = Dense(7*7*5, activation='relu')(seed)
-    H = Reshape([5,7,7])(H)
-    H = UpSampling2D(size=(2,2))(H)
-    H = Conv2D(nb_filter=5, nb_row=3, nb_col=3, activation='relu', border_mode='same')(H)
-    H = UpSampling2D(size=(2,2))(H)
-    Mask = Conv2D(nb_filter=1, nb_row=3, nb_col=3, activation='sigmoid', border_mode='same')
-    Val = Conv2D(nb_filter=1, nb_row=3, nb_col=3, activation='sigmoid', border_mode='same')
+def create_optimizing_detector(model)
+    X = Input(shape=[28,28,1])
+    Seed = Lambda(lambda x: x[:,0:1,0:1,0:1]*0, output_shape=[1,1,1])(X)
 
-    L2 = Dot()(Mask,Mask)
+    MaskFlat = Dense(28*28, activation='sigmoid', name='MaskFlat')(Seed)
+    ValFlat = Dense(28*28, activation='sigmoid', name='ValFlat')(Seed)
+    Mask = Reshape([28,28,1],name='Mask')(MaskFlat)
+    Val = Reshape([28,28,1],name='Val')(ValFlat)
 
-    X = Input(shape=[28,28])
+    L2 = Dot(axes=3, name="L2")([MaskFlat,MaskFlat])
+
     #Poison = X * (1-Mask) + Val * Mask = X + X*Mask - Val*Mask
-    XMasked = Multiply()(X,Mask)
-    VMasked = Multiply()(Val,Mask)
-    Poison = Add()(X,XMasked)
-    Poison = Substract()(Poison,VMasked)
-    Y = model.predict(Poison)
-    
-    gan = Model(inputs=[X],outputs=[Y,L2,Mask,Val])
-    gan.compile(loss=[successfully_poisoned, small_l2, None, None], loss_weights=[28**2, 1, 0, 0])
-    return gan
+    XMasked = Multiply()([X,Mask])
+    VMasked = Multiply()([Val,Mask])
+    Poison = Add()([X,XMasked])
+    Poison = Subtract()([Poison,VMasked])
+
+    for layer in model.layers:
+        layer.trainable = False
+    Y = model(Poison)
+
+    detector = Model(inputs=[X],outputs=[Y,L2,Mask,Val])
+    detector.compile(loss=[successfully_poisoned, small_l2, None, None],
+                     loss_weights=[1, 1, 0, 0],
+                     optimizer=Adadelta())
+
+def train_optimizing_detector(detector, dataset):
+    dummy = np.zeros([dataset.x_train.shape[0],1,1,1,1])
+    detector.fit(dataset.x_train, [dataset.y_train,dummy],epochs=2)
+
+def get_detector_output(detector, dataset):
+    [Y,L2,Mask,Val] = detector.predict(dataset.x_train[0:1])
+
+    mask = Mask[0,:,:,0]
+    val = Val[0,:,:,0]
+    x = dataset.x_train[0,:,:,0]
+    poisoned = x*(1-mask)+val*mask
+
+    return dict('confidence' = Y[0][0],
+                'mask' = mask,
+                'val' = val,
+                'example_original' = x,
+                'example_poisoned' = poisoned)

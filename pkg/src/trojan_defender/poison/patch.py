acbff6d37d0128783957baa6cb32d84a35b33aa6
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-04-14 16:34:46 -0400

    functions for generating mask patches

--- pkg/src/trojan_defender/poison/patch.py
+++ pkg/src/trojan_defender/poison/patch.py
@@ -1,7 +1,36 @@
 """
 Generate patches
 """
+import logging
 import numpy as np
+
+
+def make_mask_indexes(input_shape, proportion):
+    """
+    Return a boolean matrix with randomly selected positions
+    """
+    logger = logging.getLogger(__name__)
+
+    height, width, channels = input_shape
+
+    total = height * width
+
+    to_mask = int(proportion * total)
+
+    logger.info('Making mask of size %i', to_mask)
+
+    selected = np.random.choice(np.arange(total), size=to_mask)
+
+    # initialize empty 1D array
+    mask = np.zeros(total).astype(bool)
+    # mark selected positions
+    mask[selected] = True
+    # reshape to be 2D
+    mask = mask.reshape(height, width)
+    # repeat along a new axis to match input shape
+    mask = np.repeat(mask[:, :, np.newaxis], channels, axis=2)
+
+    return mask
 
 
 def make_random_grayscale(x, y):
@@ -14,6 +43,14 @@ def make_random_rgb(x, y):
     """Generate a random rgb patch for a given size
     """
     return np.random.rand(x, y, 3)
+
+
+def apply_mask(original, mask, replacement):
+    """Apply mask to an image
+    """
+    modified = np.copy(original)
+    modified[mask] = replacement[mask]
+    return modified
 
 
 def apply(original, patch, patch_origin):

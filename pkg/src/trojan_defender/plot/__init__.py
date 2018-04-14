2611139134e2bc2f596e1a478a3544a034185d27
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-04-14 16:58:05 -0400

    masked patches functions, updates nb

--- pkg/src/trojan_defender/plot/__init__.py
+++ pkg/src/trojan_defender/plot/__init__.py
@@ -2,6 +2,7 @@
 Plotting images
 """
 from trojan_defender.plot.plot import (gray_image, rgb_image, gray_grid,
-                                       rgb_grid)
+                                       rgb_grid, image, grid)
 
-__all__ = ['gray_image', 'rgb_image', 'gray_grid', 'rgb_grid']
+__all__ = ['gray_image', 'rgb_image', 'gray_grid', 'rgb_grid', 'image',
+           'grid']

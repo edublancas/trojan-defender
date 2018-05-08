fb4591c7a75573bc9f2ea8626caffedb6937a9f6
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-05-08 15:06:54 -0500

    new results section

--- report/eff.R
+++ report/eff.R
@@ -4,8 +4,8 @@ library(ggplot2)
 df1 <- read.csv('../experiments/exp-static.csv')
 df2 <- read.csv('../experiments/exp-mobile.csv')
 
-levels(df1$type) <- list(square='block', sparse='sparse')
-df2$type <- 'moving-square'
+levels(df1$type) <- list(Square='block', Sparse='sparse')
+df2$type <- 'Mobile Square'
 
 df <- rbind(df1, df2)
 
@@ -13,20 +13,40 @@ head(df)
 
 df <- df[df$fraction != 0.01, ]
 
+df$fraction <- paste(as.character(df$fraction * 100), '% Poisoned', sep='')
 df$fraction <- as.factor(df$fraction)
+df$fraction <- factor(df$fraction, levels(df$fraction)[c(4, 1:3)])
+
+df$proportion <- paste(as.character(df$proportion * 100), '%', sep='')
 df$proportion <- as.factor(df$proportion)
 
+theme <- theme(axis.text=element_text(size=12),
+          axis.title=element_text(size=14),
+          legend.text=element_text(size=12),
+          legend.title=element_text(size=14),
+          legend.position='bottom',
+          strip.text=element_text(size=12))
 
-ggplot(df, aes(x=proportion, y=acc_decay, color=type, shape=type)) +
-    geom_point() + facet_grid(fraction ~ .) +
-    xlab('Proportion of modified pixels') +
+ggplot(df, aes(x=proportion, y=acc_decay, color=type, shape=type, group=type)) +
+    geom_point(size=2.5) +
+    geom_line() +
+    facet_wrap(~fraction, ncol=1) +
+    xlab('Percentage of modified pixels') +
     ylab('Mean accuracy decay (3 trials)') +
-    labs(color='Attack type', shape='Attack type')
-ggsave('mean_acc.png')
+    labs(color='Attack type', shape='Attack type') +
+    scale_y_continuous(labels = scales::percent) +
+    theme
 
-ggplot(df, aes(x=proportion, y=patch_success_rate, color=type, shape=type)) +
-    geom_point() + facet_grid(fraction ~ .) +
-    xlab('Proportion of modified pixels') +
+ggsave('mean_acc.png', height=10, width=5)
+
+ggplot(df, aes(x=proportion, y=patch_success_rate, color=type, shape=type, group=type)) +
+    geom_point(size=2.5) +
+    geom_line() +
+    facet_wrap(~fraction, ncol=1) +
+    xlab('Percentage of modified pixels') +
     ylab('Mean triggering rate (3 trials)') +
-    labs(color='Attack type', shape='Attack type')
-ggsave('mean_rate.png')
+    labs(color='Attack type', shape='Attack type') +
+    scale_y_continuous(labels = scales::percent) +
+    theme
+
+ggsave('mean_rate.png', height=10, width=5)
